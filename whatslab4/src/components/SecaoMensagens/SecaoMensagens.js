0a5383cea0b101fc916c40cc0f7d9b9970e91bf1
gabrielMina <gabrielmina118@gmail.com>
2021-03-12 14:50:23 -0300

    secao-mensagem inserindo a mensagem

--- whatslab4/src/components/SecaoMensagens/SecaoMensagens.js
+++ whatslab4/src/components/SecaoMensagens/SecaoMensagens.js
@@ -2,17 +2,73 @@ import React from 'react'
 import styled from 'styled-components'
 import NovaMensagem from '../NovaMensagem/NovaMensagem'
 
-const Botao = styled.button`
-    color: black;
-`
+const Principal = styled.div`
+    display:flex;
+    justify-content:space-between;
+    flex-direction:column;
+    width:40%;
+    margin: 0 auto;
+    border: 1px solid black;
+    height:98vh;
+`;
+
+const MensagemPrincipal = styled.div`
+    display:flex;
+    height:100%;
+    flex-direction:column;
+    justify-content:flex-end;
+`;
+
+const Mensagem =styled.div`
+    margin-top:10px;
+    padding: 10px;
+`;
 
 export default class SecaoMensagens extends React.Component {
+
+    state = {
+        mensagens: [
+            {
+                nome:"josé isaac", 
+                mensagem: "Eai clan"
+            },
+            {
+                nome:"josé isaac", 
+                mensagem: "Eai mano"
+            }
+        ]
+    }
+    
+    
+    enviarMensagem = (mensagem) =>{
+
+        const novasMensagems = [...this.state.mensagens ,mensagem]
+
+        this.setState({mensagens:novasMensagems});
+    }
+    
     render() {
+        let mensagens;
+        
+        mensagens = this.state.mensagens.map((mensagem) =>{
+            return(
+                <Mensagem>
+                    <strong>{mensagem.nome}</strong> {mensagem.mensagem}
+                </Mensagem>
+            );
+        })
+
         return (
-            <div>
-                <Botao>Envair</Botao>
-                <NovaMensagem />
-            </div>
+            <Principal>
+                <MensagemPrincipal>
+                    {mensagens}
+                    
+                </MensagemPrincipal>
+                
+                <NovaMensagem 
+                    enviar ={this.enviarMensagem}
+                />
+            </Principal>
         )
     }
 }
